f1249de4fc25df33433069d639bdc78dd4c7313a
nquezada <nquezada@everis.com>
2020-03-04 12:37:47 -0300

    clase1

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
+/*** Vanilla JS ***/
+
 // const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
+// element.innerText = "Hello, Platzi Badges!!";
 
 // const container = document.getElementById('app');
 
 // container.appendChild(element);
 
+/*** React ***/
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+const element = <h1>Hello, Platzi Badges</h1>;
 
 const container = document.getElementById('app');
 
-// ReactDOM.render(__qué__, __dónde__);
+// ReactDOM.render(__qué__, __donde__);
 ReactDOM.render(element, container);
