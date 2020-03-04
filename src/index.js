ec32bb27aa3a295064a7b634896adfd49f538c3a
nquezada <nquezada@everis.com>
2020-03-04 12:51:09 -0300

    clase2

--- src/index.js
+++ src/index.js
@@ -11,9 +11,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges</h1>;
+//const element = <h1>Hello, Platzi Badges</h1>;
+
+// React.createElement(__tipoElemento__, __{atributos/props}__, __children__)
+//const element = React.createElement('a', {href: 'https://platzi.com'}, 'Ir a Platzi');
+//const name = 'Nadia';
+// const element = React.createElement(
+//     'h1',
+//     {},
+//     `Hola, soy ${name}`
+// );
+//const jsx = <h1>Hola soy, {name}</h1>
+
+const jsx = (
+    <div>
+        <h1>Hola, soy Nadia</h1>
+        <p>Saludos</p>
+    </div>
+);
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __donde__);
-ReactDOM.render(element, container);
+ReactDOM.render(jsx, container);
