27a3397db1e89469a3cfaf78a7738d41a91126b4
nquezada <nquezada@everis.com>
2020-03-07 19:59:11 -0300

    clase 17

--- src/index.js
+++ src/index.js
@@ -1,36 +1,16 @@
-/*** Vanilla JS ***/
-
-// const element = document.createElement('h1');
-// element.innerText = "Hello, Platzi Badges!!";
-
-// const container = document.getElementById('app');
-
-// container.appendChild(element);
-
 /*** React ***/
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-//const element = <h1>Hello, Platzi Badges</h1>;
+import "bootstrap/dist/css/bootstrap.css";
+import "./global.css";
 
-// React.createElement(__tipoElemento__, __{atributos/props}__, __children__)
-//const element = React.createElement('a', {href: 'https://platzi.com'}, 'Ir a Platzi');
-//const name = 'Nadia';
-// const element = React.createElement(
-//     'h1',
-//     {},
-//     `Hola, soy ${name}`
-// );
-//const jsx = <h1>Hola soy, {name}</h1>
+import BadgeNew from "./pages/BadgeNew";
 
-const jsx = (
-    <div>
-        <h1>Hola, soy Nadia</h1>
-        <p>Saludos</p>
-    </div>
-);
-
-const container = document.getElementById('app');
+const container = document.getElementById("app");
 
 // ReactDOM.render(__qué__, __donde__);
-ReactDOM.render(jsx, container);
+ReactDOM.render(
+  <BadgeNew/>,
+  container
+);
